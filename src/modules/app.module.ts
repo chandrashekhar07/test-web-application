132164c59b861f7bc3d760366574677d3e3a982a
chandra shekhar <shekharneupane23@gmail.com>
2022-03-25 22:29:00 +0545

    linting issue resolve

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -11,27 +11,27 @@ import { SalesModule } from './sales/sales.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRoot({
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            type: 'sqlite' as any,
-            database: configProvider.useFactory().DATABASE_NAME,
-            synchronize: configProvider.useFactory().DATABASE_SYNCHRONIZE,
-            entities: [configProvider.useFactory().DATABASE_ENTITIES]
-        }),
-        CommonModule,
-        CountryModule,
-        ProductModule,
-        SalesModule,
-        TaskModule
-    ],
-    providers: [
-        {
-            provide: APP_FILTER,
-            useClass: HttpExceptionsFilter
-        },
-        CommonModule
-    ]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      type: 'sqlite' as any,
+      database: configProvider.useFactory().DATABASE_NAME,
+      synchronize: configProvider.useFactory().DATABASE_SYNCHRONIZE,
+      entities: [configProvider.useFactory().DATABASE_ENTITIES]
+    }),
+    CommonModule,
+    CountryModule,
+    ProductModule,
+    SalesModule,
+    TaskModule
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionsFilter
+    },
+    CommonModule
+  ]
 })
 export class ApplicationModule {}
